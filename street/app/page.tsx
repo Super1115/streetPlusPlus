0340abb19511eeb307aa172705c5a78c0b08bec0
= <chuangsuper@gmail.com>
2024-11-21 23:48:17 +0800

    edit home page

--- street/app/page.tsx
+++ street/app/page.tsx
@@ -1,9 +1,12 @@
-import "/app/page.css";
+import "/app/page.module.css";
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
 import userIcon from "/public/account_circle.svg"
 export default function Home() {
+  const router = useRouter()
   return (
-    <button className="userButton">
+    <button className="userButton" onClick={() => router.push('/profile')}>
     <Image src={userIcon} alt="User" className="user-icon" />
   </button>
   );
